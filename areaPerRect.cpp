fa3b516d607b6392f0887e7445390af7897791af
Ubuntu <ubuntu@ip-172-31-91-42.ec2.internal>
2021-11-26 18:01:27 +0000

    Created areaPerRect.cpp and basicMath.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Year Your Name All rights reserved.
+// Created by: Logan Sweeney
+// Created on: November 25, 2021
+// This program does basic math.
+#include <iostream>
+
+int main() {
+  std::cout << "For a rectangle that has dimensions\n";
+  std::cout << "length 6cm and width 7cm:\n";
+  std::cout << "\n";
+  std::cout << "Area = " << 6 * 7 << " cm^2\n";
+  std::cout << "Perimeter = " << 2* (6 + 7) << " cm\n";
+}
---
+++ basicMath.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) Year Your Name All rights reserved.
+// Created by: Logan Sweeney
+// Created on: November 25, 2021
+// This program does basic math.
+#include <iostream>
+#include <cmath>
+
+int main() {
+  std::cout << "99 + 6 = " << 99+6 << std::endl;
+  std::cout << "333 - 2946 = " << 333-2946 << std::endl;
+  std::cout << "8967 / 7 = " << 8967/7 << std::endl;
+  std::cout << "92836 - 2395 / 2 = " << 92836-2395/2 << std::endl;
+  std::cout << "2^3 = " << pow(2, 3) << std::endl;
+  std::cout << "1 / 3 = " << float(1)/float(3) << std::endl;
+}
